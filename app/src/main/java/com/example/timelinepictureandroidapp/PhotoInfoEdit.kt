cc65fa5011d35c4dd8674caa6ca369b615a72fcd
mikaely <mikael.ylivaara@metropolia.fi>
2021-03-03 13:47:00 +0200

    more flow

--- app/src/main/java/com/example/timelinepictureandroidapp/PhotoInfoEdit.kt
+++ app/src/main/java/com/example/timelinepictureandroidapp/PhotoInfoEdit.kt
@@ -1,7 +1,10 @@
 package com.example.timelinepictureandroidapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.photo_info_edit_layout.*
 
 class PhotoInfoEdit : Fragment(R.layout.fragment_photo_info_edit) {
 
@@ -10,7 +13,28 @@ class PhotoInfoEdit : Fragment(R.layout.fragment_photo_info_edit) {
 
     }
 
-    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        photo_info_edit_save_button.setOnClickListener {
+            DataInDB.name = photo_info_edit_edit_name.toString()
+            DataInDB.info = photo_info_edit_edit_info.toString()
+          //  DataInDB.setplace()
+            val intent = Intent(requireContext(),MainActivity::class.java)
+            startActivity(intent)
+        }
+        photo_info_edit_retake_button.setOnClickListener {
+
+                val transaction = activity?.supportFragmentManager?.beginTransaction()
+                if (transaction != null) {
+                    transaction.replace(R.id.fcView, PhotoFragment())
+
+                    transaction.disallowAddToBackStack()
+                    transaction.commit()
+                }
+        }
+
+    }
 
 
 }
