1fb80b91dd12c6f6fcffb442d7ba08aab17e11f7
mikaely <mikael.ylivaara@metropolia.fi>
2021-03-09 15:09:22 +0200

    googlemaps bounds

--- app/src/main/java/com/example/timelinepictureandroidapp/MapActivity.kt
+++ app/src/main/java/com/example/timelinepictureandroidapp/MapActivity.kt
@@ -1,19 +1,18 @@
 package com.example.timelinepictureandroidapp
 
-import android.location.Location
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.FragmentActivity
 import com.example.timelinepictureandroidapp.db.PlaceDB
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
+
 
 class MapActivity : FragmentActivity(),OnMapReadyCallback {
 
@@ -25,7 +24,7 @@ class MapActivity : FragmentActivity(),OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        GlobalScope.async(Dispatchers.IO ) {
+        GlobalScope.async(Dispatchers.IO) {
             places = db.placeDao().getAll()
         }
         val mapFragment = supportFragmentManager.findFragmentById(R.id.frMap) as SupportMapFragment
@@ -33,27 +32,23 @@ class MapActivity : FragmentActivity(),OnMapReadyCallback {
     }
 
     override fun onMapReady(p0: GoogleMap) {
-        var maxLat = -90.0
-        var minLat = 90.0
-        var maxLong = -180.0
-        var minLong = 180.0
+        val latlngBuilder = LatLngBounds.Builder()
+
        map = p0.apply {
            for (place in places) {
-               if (place.latitude<minLat) minLat = place.latitude
-               if (place.latitude>maxLat) maxLat = place.latitude
-               if (place.longitude<minLong) minLong = place.longitude
-               if (place.latitude>maxLong) maxLong = place.longitude
+               latlngBuilder.include(LatLng(place.latitude,place.longitude))
 
                addMarker(
                        MarkerOptions()
-                               .position(LatLng( place.latitude,place.longitude))
+                               .position(LatLng(place.latitude, place.longitude))
                                .title(place.name.toString())
                                .snippet(place.info.toString())
 
                )
            }
        }
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng((maxLat+minLat)/2,(maxLong+minLong)/2),5f))
+
+        map.animateCamera(CameraUpdateFactory.newLatLngBounds(latlngBuilder.build(),100))
     }
 
 
